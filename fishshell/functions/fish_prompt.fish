3309578cd44ddec8fd1154039131230c31cfaa45
Gavin Heverly-Coulson <gavin@quantumgeranium.com>
2021-05-21 15:26:52 -0700

    Update fish prompt
    use prompt_pwd command and remove $status component

--- fishshell/functions/fish_prompt.fish
+++ fishshell/functions/fish_prompt.fish
@@ -1,16 +1,12 @@
+# Note: To get the prompt to show full directory names of prompt_pwd need to add
+# set -g fish_prompt_pwd_dir_length 0
+# to ~/.config/fish/config.fish
+
 function fish_prompt --description 'Write out the prompt'
     # Log previous command to history log file
     log_fish_history
 
-    # Determine exit status of last command
-    set stat $status
-    # Set status color
-    set status_color (set_color -o $fish_color_comment)
-    if test $stat -gt 0
-        set status_color (set_color -o $fish_color_error)
-    end
-
     # echo the info prompt
-    echo (set_color $fish_color_redirection)"["(date "+%H:%M:%S")"] "(set_color -o $fish_color_parameter)$USER@(hostname -s) (set_color -o $fish_color_command)(pwd) $status_color\($stat\)(set_color normal)
+    echo (set_color $fish_color_redirection)"["(date "+%H:%M:%S")"] "(set_color -o $fish_color_parameter)$USER@(hostname -s) (set_color -o $fish_color_command)(prompt_pwd)
     echo (set_color -o $fish_color_comment)" \$ "(set_color normal)
 end
