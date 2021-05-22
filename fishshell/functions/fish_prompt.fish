886bfee55d16edd57c3e16d72fe3a731a9f64e43
Gavin Heverly-Coulson <gavin@quantumgeranium.com>
2021-05-21 17:08:51 -0700

    revamp fish_prompt function to be more readable, include git info

--- fishshell/functions/fish_prompt.fish
+++ fishshell/functions/fish_prompt.fish
@@ -1,12 +1,29 @@
-# Note: To get the prompt to show full directory names of prompt_pwd need to add
-# set -g fish_prompt_pwd_dir_length 0
-# to ~/.config/fish/config.fish
-
+# Looks like:
+# [HH:MM:SS] (gitbranch) ~/current/directory
+#  $ 
 function fish_prompt --description 'Write out the prompt'
     # Log previous command to history log file
     log_fish_history
 
-    # echo the info prompt
-    echo (set_color $fish_color_redirection)"["(date "+%H:%M:%S")"] "(set_color -o $fish_color_param)$USER@(hostname -s) (set_color -o $fish_color_command)(prompt_pwd)
-    echo (set_color -o $fish_color_comment)" \$ "(set_color normal)
+    # Display current time
+    set_color $fish_color_redirection
+    printf '[%s]' (date "+%H:%M:%S")
+
+    # Show current git branch, if not in a git repo, show username@hostname instead
+    set_color -o $fish_color_param
+    fish_git_prompt
+    if test $status -ne 0
+        printf ' %s@%s ' $USER (hostname -s)
+    else
+        printf ' '
+    end
+
+    # Display current working directory, with a newline
+    set_color -o $fish_color_command
+    echo (prompt_pwd)
+
+    # Give final prompt character
+    set_color -o $fish_color_comment
+    echo -n ' $ '
+    set_color normal
 end
